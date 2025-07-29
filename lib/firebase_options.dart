1c124947cf0088bb9154cc41be1025bc34538675
BederCC <beder1810@gmail.com>
2025-07-29 01:14:38 -0500

    mejoras

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -53,7 +53,7 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBzbolZRScYipZ0WFrxPcaKUo0mH4lUUxA',
+    apiKey: '?',
     appId: '1:1038269159201:android:9c1ad2f82247d57b065060',
     messagingSenderId: '1038269159201',
     projectId: 'aplicacion-luz',
